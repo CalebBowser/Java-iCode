54809e6aab03c4cd9e6c9afecf93e96a959cf47a
NinjaBob <153332837+NinjaBob123@users.noreply.github.com>
2024-09-07 13:54:01 -0500

    Update UserAuth.java

--- UserAuth.java
+++ UserAuth.java
@@ -7,21 +7,24 @@ public class UserAuth {
 
         if(userNameAuth(userName)){
             System.out.println("Hello " + userName + " Please enter your password:");
-            String userPass = userInput.nextLine();
-            if(userPassAuth(userPass)){
-                System.out.println("Password ACCEPTED: Logging ON.");
-            }
-            else{
-                System.out.println("Incorrect Password");
+            for (int i=0; i<=3; i++){
+                String userPass = userInput.nextLine();
+                if(userPassAuth(userPass)){
+                    System.out.println("Password ACCEPTED: Logging ON.");
+                    break;
+                }
+                else{
+                    System.out.println("Incorrect Password");
+                    System.out.println("Please try again");
+                    System.out.println("You have " + String.valueOf(3-i) + " more tries");
+                }
             }
         }
         else{
             System.out.println("NO user found.");
         }
-
         userInput.close();
     }
-
     public static Boolean userNameAuth(String uName){
         if(uName.equals("Kev45")){
             return true;
@@ -30,7 +33,6 @@ public class UserAuth {
             return false;
         }
     }
-
     public static Boolean userPassAuth(String uPass){
         if(uPass.equals("password")){
             return true;
