6d45ff656cf360a1b0385ea7e0bd1cc8112048eb
NinjaBob <153332837+NinjaBob123@users.noreply.github.com>
2024-09-07 13:18:13 -0500

    Update Basic_Input.java (#5)

--- Basic_Input.java
+++ Basic_Input.java
@@ -2,12 +2,15 @@ import java.util.Scanner; //Import Scanner Tool
 
 public class Basic_Input{
     public static void main(String[] args){
+        //Creating a new in-flow reader to watch for inputs
         Scanner myScanner = new Scanner(System.in);
         System.out.println("what is your name?");
-
+        //                  ^^^^^^^^^^^^^^^^^^
+        //Getting the user's response to the ^ question
         String userName = myScanner.nextLine();
+        //Greeting the user with inputed name
         System.out.println("Hello " + userName);
-
+        //Closing the scanner so that it doesn't take up memory
         myScanner.close();
     }
 }
