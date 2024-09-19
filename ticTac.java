8bf790fec54e1491894feb3b76a4d34f5df8275a
NinjaBob123 <153332837+NinjaBob123@users.noreply.github.com>
2024-09-19 15:44:24 -0500

    Update ticTac.java

--- ticTac.java
+++ ticTac.java
@@ -1,11 +1,15 @@
 public class ticTac {
     public static void main(String[] args){
-        
+        char[][] state = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+        boardState(state);
     }
 
-    public static void boardState(String[][] state){
-        for (String[] row : state){
-            print(row[0] + "|" + row[1] + "|" + row[2]);
+    public static void boardState(char[][] state){
+        for (char[] row : state){
+            String char1 = row[0] + "\u0332";
+            String char2 = row[1] + "\u0332";
+            String char3 = row[2] + "\u0332";
+            print(char1 + "|" + char2 + "|" + char3);
         }
     }
     public static void print(String val){
