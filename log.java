81855e2903f4cbf124b3d503639c43bd41fb7d40
NinjaBob <153332837+NinjaBob123@users.noreply.github.com>
2024-09-25 19:22:33 +0000

    Add log.java

---
+++ log.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class log {
+    public static void main(String[] args) {
+        Scanner inner = new Scanner(System.in);
+        String stop = "no";
+        while (stop == "no"){
+            System.out.println("What would like to log?: ");
+            int getLog = inner.nextInt();
+            System.out.println(getLog^2^-2);
+        }
+    }
+}
