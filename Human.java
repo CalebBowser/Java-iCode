9c8fb1e8273cd0fdee22508421dbd657bb473ae1
CalebBowser <54370867+CalebBowser@users.noreply.github.com>
2024-09-28 13:55:48 -0500

    Add files via upload
    This is for reference when creating your own human class and starting subclasses.

---
+++ Human.java
@@ -0,0 +1,23 @@
+public abstract class Human {
+    private String name;
+    private int age;
+    private String gender;
+
+    //Constructor
+    public Human(String name_, int age_, String gender_){
+        this.name = name_;
+        this.age = age_;
+        this.gender = gender_;
+    }
+
+    //Getters
+    public String getName(){return name;}
+    public int getAge(){return age;}
+    public String getGender(){return gender;}
+
+    //Setters
+    public void setName(String name_){this.name = name_;}
+    public void setAge(int age_){this.age = age_;}
+    public void setGender(String gender_){this.gender = gedner_;}
+
+}
